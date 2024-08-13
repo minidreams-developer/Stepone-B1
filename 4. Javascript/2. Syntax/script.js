997231885fccbc416dd9d3b8c757f172d0c4c341
arunprince-minidreams <arunprince@minidreams.in>
2024-08-13 11:47:57 +0530

    upto interaction method completed

---
+++ 4. Javascript/2. Syntax/script.js
@@ -0,0 +1,27 @@
+// Three types to declare a variable;
+// apple
+// intializing
+// declaring
+let box1 = "apple"; // variable initialized and declared;
+
+console.log("before change===>", box1);
+
+// redeclare
+box1 = "orange";
+
+console.log("after change====>", box1);
+
+//--------------------------------------------------------
+// let and var
+// variable initialized
+let box2;
+// value declared
+box2 = "mango";
+
+// needs to be intialized and declared at same line
+const box3 = "RED";
+
+// box3 = "pinapple";
+
+// redeclaring
+// same variable unique ( like id in css )
---
+++ 4. Javascript/3. Interaction method/script.js
@@ -0,0 +1,18 @@
+// Interaction Method
+// 1. console.log("value")
+console.log("Hi");
+
+// 2. alert()
+// alert("hi, this is an alert");
+
+// 3. prompt("message", default value)
+// let firstName = prompt("Tell me your name?", "Arun");
+
+// let lastName = prompt("Tell me your last name", "Prince");
+
+// console.log("Full Name", firstName + lastName);
+
+// 4. confirm
+let isAdult = confirm("Are you 14+ years old ?");
+
+console.log("is Adult ==>", isAdult);

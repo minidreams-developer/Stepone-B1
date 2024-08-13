28b915307e4c54ab23e0d8f61b480ccebe17ff6d
arunprince-minidreams <arunprince@minidreams.in>
2024-08-13 12:07:15 +0530

    Number Data types compelted

---
+++ 4. Javascript/4. DataTypes/script.js
@@ -0,0 +1,34 @@
+// To store a value in a variable
+// Datatype differs for each languages
+// Data Types
+// Two categories
+// 1. Primitive Data types
+//        i) Number, ii) BigInt, iii) String, iv) Boolean v) null, vi) undefined, vii) symbol
+
+// 2. Non-primitive Data types
+//        i) Object, ii) Array, iii) functions
+
+// Primitive Data Types
+// i) Number
+//    whole Number, decimal Number, infinity, -inifinity, NaN
+
+let num = 13;
+let cash = 13.9;
+let unlimited = Infinity;
+let negativeUnlimited = -Infinity;
+
+console.log("num", num);
+console.log("num data type===>", typeof num);
+console.log("=================================");
+
+console.log("Decimal", cash);
+console.log("Decimal cash data type===>", typeof cash);
+console.log("=================================");
+
+console.log("infinity", unlimited);
+console.log("infinity data type===>", typeof unlimited);
+console.log("=================================");
+
+console.log("Negative Infinity", negativeUnlimited);
+console.log("Negative Infinity data type===>", typeof negativeUnlimited);
+console.log("=================================");

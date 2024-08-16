ac88e96e7e869199813b555d0b1929d3475d8c03
arunprince-minidreams <arunprince@minidreams.in>
2024-08-16 14:33:06 +0530

    Basic operators completed

---
+++ 4. Javascript/5. Type conversion/typeConversion.js
@@ -0,0 +1,41 @@
+// Number conversion
+// convert other data types into number
+
+// let age = "23";
+// console.log("Age->", age);
+// console.log("type of Age->", typeof age);
+
+// age = Number(age);
+
+// console.log(" after converion Age->", age);
+// console.log("after converion type of Age->", typeof age);
+
+// rules
+/*
+  undefined => NaN
+  null      => 0
+  true/false => 1 / 0
+  string   => NaN
+  Numerical String => Number
+*/
+
+// let name = "Jayamani";
+// name = Number(name);
+// console.log("name-->", name);
+// console.log("type of name-->", typeof name);
+
+// Boolean conversion
+/*
+    false => 0, "", null, undefined, NaN
+    true => everything else is true
+*/
+
+let age = 0;
+// age = Boolean(age);
+console.log("Age---", age);
+
+// if (age) {
+//   console.log("Age is true");
+// } else {
+//   console.log("age is not true");
+// }
---
+++ 4. Javascript/6. Operators/script.js
@@ -0,0 +1,49 @@
+// Adding two values
+// 2(value1) + 2(value2) = 4;
+// operands => 2, 4
+// operator  => +
+// expression => value1 + value2 expression
+// unary operator => +a
+// binary operator => two operands eg: a + b
+
+// Basic Arithmetic operators
+// +, -, *, / , %, **
+// Addition => +
+// Subtraction => -
+// Multiplication => *
+// division => /
+// remainder => %
+// Exponention => **
+
+console.log("Addition", 10 + 20);
+console.log("Subtraction", 10 - 20);
+console.log("Multiplication0", 10 * 20);
+console.log("division", 10 / 20);
+console.log("remainder", 10 % 20);
+console.log("exponential", 2 ** 2);
+
+// Operator precedence
+// BODMAS
+console.log("multiple expressions", 10 + 2 / 6 - 3);
+
+// String concat
+console.log("string concat", "hello" + "World");
+console.log("concat", 1 + "hello");
+
+// Assignment operators
+// = assignment
+let area = "chennai";
+
+// binary operators
+let num1 = 10;
+let num2 = 20;
+let value = num1 + num2;
+// unary operators
+
+let num0 = 12;
+
+// increament operator
+++num0;
+console.log("num0e", num0);
+--num0;
+console.log("after decrement", num0);

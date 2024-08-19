5fab1e7a57892894cdbcd69b8817860bc505b340
arunprince-minidreams <arunprince@minidreams.in>
2024-08-19 12:06:40 +0530

    Conditional branching started

---
+++ 4. Javascript/8. conditional Branching/script.js
@@ -0,0 +1,55 @@
+// if class
+// what is blocks of code?
+/*
+
+block
+{
+// multiples lines of codes
+let name = "prince"
+
+}
+
+// you cannot access the block scoped variable outside the block
+*/
+
+// if(condition){
+
+// multiple statement
+//        }
+
+let num1 = 10;
+let num2 = 20;
+
+if (num1 > num2) {
+  console.log("True, 10 is less than 20");
+  console.log(" if true, if block executed");
+} else {
+  console.log("false, else block gets executed");
+}
+
+// if..else
+
+// variable declaration
+// let name = "john"
+// variable name should be descriptive
+// let userName or let currentUserName =
+
+// camelCase
+//currentUserName
+//firstName
+// dateOfBirth
+
+// CAPS
+const BIRTHDATE = "25-08-2005";
+
+//PASCAL CASE
+// CurrentUserName
+
+// camel case
+// let currentUserName = "john";
+
+// pascel case
+// let CurrentUserName = "john";
+
+//caps
+const CURRENTUSERNAME = "john";

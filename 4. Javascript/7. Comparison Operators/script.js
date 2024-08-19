0b467bdceada7de8b5866b4b75e7cea74bb3adee
arunprince-minidreams <arunprince@minidreams.in>
2024-08-19 11:30:43 +0530

    Comparison operator completed

---
+++ 4. Javascript/7. Comparison Operators/script.js
@@ -0,0 +1,38 @@
+// Comparison Operators
+// Less than =>                      <
+// Greater than =>                   >
+// equals to   =>                    ==
+// Greater than or equals to  =>     >=
+// Less than or equals to   =>       <=
+// not equals to     =>              !=
+// Strict equals to  =>              ===
+
+// It compares two values and output true/false
+
+// less than
+let num1 = 10;
+let num2 = 20;
+
+// console.log("10 < 20 = ", num1 < num2);
+
+// // Greater than
+// console.log("10 > 20 = ", num1 > num2);
+
+// less than or equals to
+// console.log("10 <= 10 = ", num1 < num2);
+
+// // greater than or equals to
+// console.log("10 >= 20 = ", num1 >= num2);
+
+// if we compare different data types , the value will be converted into number data types
+// equals to
+// console.log("5 == 5", 5 == "5");
+// console.log("1 == true", 1 == true); // true = 1
+
+// // not equals to
+// console.log("10 != 10", 10 != 10);
+
+//Strict equels to
+// it will also compare the datatypes
+// same value and same data types
+console.log("5 === true", 1 === true);

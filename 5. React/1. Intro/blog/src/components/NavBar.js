45c4618375e893e259bd90596342e02b1ba23bf9
arunprince-minidreams <arunprince@minidreams.in>
2024-09-14 17:10:54 +0530

    React intro

---
+++ 5. React/1. Intro/blog/src/components/NavBar.js
@@ -0,0 +1,12 @@
+const NavBar = () => {
+  let name = "john";
+  //============================================
+  return (
+    // UI PART html comes here
+    <div className="nav">
+      <h2>Nav bar comes here</h2>
+    </div>
+  );
+};
+
+export default NavBar;
